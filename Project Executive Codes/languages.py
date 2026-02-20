bca20c0978c458e178d481e0551775e3e107cf38
Tejaswini302005 <chandaluritejaswini30@gmail.com>
2026-02-20 23:45:21 +0530

    Add files via upload

---
+++ Project Executive Codes/languages.py
@@ -0,0 +1,18 @@
+languages = [
+
+    "English",
+    "Hindi",
+    "Telugu",
+    "Tamil",
+    "Kannada",
+    "Malayalam",
+    "French",
+    "German",
+    "Spanish",
+    "Japanese",
+    "Chinese",
+    "Korean",
+    "Arabic",
+    "Russian"
+
+]
